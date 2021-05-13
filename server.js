d59fe0cb99d04503ff357b467bd118b3b8269dc5
Afraz Malik <afrazmalik321@gmail.com>
2021-05-14 00:04:40 +0500

    'changin'

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const cors = require('cors');
 if (process.env.NODE_ENV !== 'production') require('dotenv').config();
 
-const stripe = require('stripe')(process.env.stripe.key);
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 const app = express();
 const port = process.env.PORT || 5000;
