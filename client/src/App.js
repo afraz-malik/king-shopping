9c7cfa4fa4279000dcb02335ea661f82e029a925
Afraz Malik <afrazmalik321@gmail.com>
2021-05-16 18:53:27 +0500

    Making RWD

--- client/src/App.js
+++ client/src/App.js
@@ -17,6 +17,8 @@ import Header from './components/header/header';
 import HomePage from './pages/homepage/homepage';
 import ShopPage from './pages/shoppage/shoppage';
 import CheckOut from './pages/checkout/checkout'
+import AboutPage from './pages/about/about'
+
 import SignInSignUp from './pages/sign-in-sign-up/sign-in-sign-up';
 //Components
 import Spinner from './components/spinner/spinner'
@@ -50,6 +52,7 @@ const App = ({ currentUser, isCartHidden, isFetching, gettingShopData_Start, isU
       <Switch>
         <Route exact path={`${URL}`} render={props => <HomepageWithSpinner isLoading={isFetching} {...props} />} />
         <Route path={`${URL}shop`} render={props => <ShopPageWithSpinner isLoading={isFetching} {...props} />} />
+        <Route path={`${URL}about`} component={AboutPage} />
         <Route exact path={`${URL}checkout`} component={CheckOut} />
         <Route exact path={`${URL}signIn`} render={() => currentUser ? (<Redirect to={`${URL}`} />) : <SignInSignUp />} />
       </Switch>
---
+++ client/src/pages/about/about.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const homepage = () =>{
+    return(
+        <div className='about'>
+            Page Under Construction ;)
+        </div>
+    )
+}
+
+export default homepage;
