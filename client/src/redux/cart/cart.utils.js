91cd197a263a0265c9c97c133a41041fa4cd0753
Afraz Malik <afrazmalik321@gmail.com>
2021-05-28 00:32:27 +0500

    Adding admin

--- client/src/redux/cart/cart.utils.js
+++ client/src/redux/cart/cart.utils.js
@@ -1,11 +1,12 @@
 export const addToCart = (cartItem, NewItem) =>{
     const existItem = cartItem.find(
-        item => item.id === NewItem.id
+        item =>  item.id === NewItem.id && item.name === NewItem.name
+        
     );
 
     if (existItem){
         return cartItem.map(item =>
-            item.id === NewItem.id
+            item.id === NewItem.id  && item.name === NewItem.name
             ? ({...item, quantity: item.quantity+1})    
             : item
         )
@@ -14,19 +15,19 @@ export const addToCart = (cartItem, NewItem) =>{
 }
 export const clearFromCart = (cartItem, itemToRemove) =>{
     return cartItem.filter(
-        cartItem => cartItem.id !== itemToRemove.id
+        cartItem => cartItem.name !== itemToRemove.name
     )
 }
 export const decFromCart = (cartItem, NewItem)=>{
     const existItem = cartItem.find(
-        item => item.id === NewItem.id
+        item => item.name === NewItem.name
     );
     if(NewItem.quantity === 1)
         return clearFromCart(cartItem, NewItem)
     
     if (existItem){
         return cartItem.map(item =>
-            item.id === NewItem.id
+            item.id === NewItem.id  && item.name === NewItem.name
             ? ({...item, quantity: item.quantity-1})    
             : item
         )
