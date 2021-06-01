63173579dfd6338dc73dac73c20b26e560b1e10d
Afraz Malik <afrazmalik321@gmail.com>
2021-06-01 22:02:08 +0500

    Adding PWA

--- client/src/index.js
+++ client/src/index.js
@@ -7,6 +7,7 @@ import reportWebVitals from './reportWebVitals';
 import {Provider} from 'react-redux';
 import {store, persistor} from './redux/store'
 import {PersistGate} from 'redux-persist/integration/react'
+import  registerServiceWorker from './registerServiceWorker'
 
 ReactDOM.render(
   <Provider store={store}>
@@ -25,3 +26,4 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+registerServiceWorker();
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 const cors = require('cors');
+const enforce = require('express-sslify');
 if (process.env.NODE_ENV !== 'production') require('dotenv').config();
 
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
@@ -12,7 +13,7 @@ const port = process.env.PORT || 5000;
 app.use(cors())
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
+app.use(enforce.HTTPS({trustProtoHeader: true}));
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, 'client/build')));
 
@@ -25,6 +26,10 @@ app.listen(port, error => {
   if (error) throw error;
   console.log('Server running on port ' + port);
 });
+
+app.get('/service-worker.js',(req, res)=>{
+  res.sendFile(path.resolve(__dirname, '..', 'build', 'service-worker.js'));
+})
 app.post('/payment', (req, res) => {
   const body = {
     source: req.body.tokenId,
